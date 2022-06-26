23e20f9025d8fe06adec26a9813ae1986f4ff2a0
Denis-Prime <deniscode123@gmail.com>
2022-06-27 00:11:20 +0700

    adddd

---
+++ clone web react/ex3/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import CardBody from './Component/Web';
+function App() {
+  return (
+    <div className="App">
+      <CardBody/>
+    </div>
+  );
+}
+
+export default App;
---
+++ clone web react/ex3/src/Component/Web.jsx
@@ -0,0 +1,101 @@
+
+const CardBody = ()=>{
+    return(
+        <div className="over">
+            <div className="navbar">
+                <div id="fashion">FASHION</div>
+                <div id="home">Home</div>
+                <div id="article">article</div>
+                <div id="contact">Contact</div>
+                <div id="purchase">Purchase</div>
+            </div>
+            <div className="titleImg">
+                <div className="vehicle">VEHICLE</div>
+                <div className="quotes">
+                    One of Saturn’s largest rings may be newer than anyone
+                </div>
+                <div className="date">June 6, 2019    By Rickie Baroch    4 comments</div>
+            </div>
+            <div className="content">
+                <div className="left">
+                    <div className="leftContent">
+                        <div className="contentItem">
+                            <div className="img"></div>
+                            <div className="name">TOURISM</div>
+                            <div className="comment">One of Saturn’s largest rings may be newer than anyone</div>
+                            <div className="dateItem">June 6, 2019    By Rickie Baroch</div>
+                        </div>
+                        <div className="contentItem">
+                            <div className="img1"></div>
+                            <div className="name">TOURISM</div>
+                            <div className="comment">One of Saturn’s largest rings may be newer than anyone</div>
+                            <div className="dateItem">June 6, 2019    By Rickie Baroch</div>
+                        </div>
+                        <div className="contentItem">
+                            <div className="img2"></div>
+                            <div className="name">TOURISM</div>
+                            <div className="comment">One of Saturn’s largest rings may be newer than anyone</div>
+                            <div className="dateItem">June 6, 2019    By Rickie Baroch</div>
+                        </div>
+
+                        <div className="contentItem">
+                            <div className="img2"></div>
+                            <div className="name">TOURISM</div>
+                            <div className="comment">One of Saturn’s largest rings may be newer than anyone</div>
+                            <div className="dateItem">June 6, 2019    By Rickie Baroch</div>
+                        </div>
+                        <div className="contentItem">
+                            <div className="img"></div>
+                            <div className="name">TOURISM</div>
+                            <div className="comment">One of Saturn’s largest rings may be newer than anyone</div>
+                            <div className="dateItem">June 6, 2019    By Rickie Baroch</div>
+                        </div>
+                        <div className="contentItem">
+                            <div className="img1"></div>
+                            <div className="name">TOURISM</div>
+                            <div className="comment">One of Saturn’s largest rings may be newer than anyone</div>
+                            <div className="dateItem">June 6, 2019    By Rickie Baroch</div>
+                        </div>
+                    </div>
+                    <div className="contentItemBig">
+                            <div className="imgBig"></div>
+                            <div className="nameBig">TOURISM</div>
+                            <div className="commentBig">One of Saturn’s largest rings may be newer than anyone</div>
+                            <div className="dateItemBig">June 6, 2019    By Rickie Baroch</div>
+                        </div>
+                </div>
+                
+                <div className="rightContent">
+                    <div className="contentItemR">
+                        <div className="img"></div>
+                        <div className="nameRight">Kate Willems</div>
+                        <div className="downName">Food and cooking bloger</div>
+                        <div className="contentItemRight">Hi, I'm Sonia. Cooking is the way I express my creative side to the world. Welcome to my Kitchen Corner on…</div>
+                        <div className="btn">Continue Reading</div>
+                    </div>
+                    <div className="featured">Featured posts</div>
+                    <div className="contentItemR">
+                        <div className="img1R"></div>
+                        <div className="name">TOURISM</div>
+                        <div className="comment">One of Saturn’s largest rings may be newer than anyone</div>
+                        <div className="dateItem">June 6, 2019    By Rickie Baroch</div>
+                    </div>
+                    <div className="contentItemR">
+                        <div className="img2R"></div>
+                        <div className="name">TOURISM</div>
+                        <div className="comment">One of Saturn’s largest rings may be newer than anyone</div>
+                        <div className="dateItem">June 6, 2019    By Rickie Baroch</div>
+                    </div>
+                    <div className="contentItemR">
+                        <div className="img3R"></div>
+                        <div className="name">TOURISM</div>
+                        <div className="comment">One of Saturn’s largest rings may be newer than anyone</div>
+                        <div className="dateItem">June 6, 2019    By Rickie Baroch</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    )
+}
+export default CardBody
